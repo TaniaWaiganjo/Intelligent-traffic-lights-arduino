96196a0f18fd42aa568efd8b28c3ae0aa33d1360
TaniaWaiganjo <85108230+TaniaWaiganjo@users.noreply.github.com>
2023-04-03 08:59:32 +0300

    Update fisfuzzy2inp.ino

--- fisfuzzy2inp/fisfuzzy2inp.ino
+++ fisfuzzy2inp/fisfuzzy2inp.ino
@@ -1,9 +1,5 @@
 //***********************************************************************
-// Matlab .fis to arduino C converter v2.0.1.25122016                   
-// - Karthik Nadig, USA                                                  
-// Please report bugs to:                                                
-// https://github.com/karthiknadig/ArduinoFIS/issues                     
-// If you don't have a GitHub account mail to karthiknadig@gmail.com     
+// Matlab .fis to arduino C converter v2.0.1.25122016                        
 //***********************************************************************
 
 #include "fis_header.h"
